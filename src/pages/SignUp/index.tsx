f2add97c7daf5d5799f3c1f575a12b126cee76dd
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-02 20:47:55 -0300

    Using @unform to handle forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="nome" placeholder="Nome" icon={FiUser} />
           <Input name="email" placeholder="E-mail" icon={FiMail} />
@@ -22,7 +26,7 @@ const SignUp: React.FC = () => {
             icon={FiLock}
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para logon

9807d9ca1e9b869861cd20591ead7b0850811418
João Giovanni <joaogiovannivilela@gmail.com>
2020-12-01 20:20:09 -0300

    Created Dashboard header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,11 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/apiClient';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 interface SignInCredentials {
   email: string;
   password: string;
@@ -8,11 +13,11 @@ interface SignInCredentials {
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
